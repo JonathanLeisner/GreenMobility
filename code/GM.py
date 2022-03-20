839d6cd4a68917d500aacd91c39d068485638448
JonathanLeisner <42312385+JonathanLeisner@users.noreply.github.com>
2022-03-20 18:20:23 -0500

    V2: First solve complete. No simulation yet

--- code/GM.py
+++ code/GM.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from types import SimpleNamespace
-
+np.set_printoptions(precision=2)
 
 #%%
 
@@ -25,15 +25,19 @@ class GM:
         par.a_min = 30 #youngest cohort
         par.ages = np.arange(par.a_min, par.a_max + 1)
         par.N_ages = par.a_max + 1 - par.a_min
+        
+        
+        #Not used
         par.exper_min = 1e-6 # minimum point in grid for experience
         par.exper_max = 20.0 # maximum point in grid for experience
         par.gridpoints_exper = 200 #number of gridpoints for experience
 
         #Parameters that are calibrated
         par.rho = 0.96
+        par.sigma = 1
 
         #Parameters to estimate later
-        par.beta0 = np.array([0.04, 0.05, 0.06]) #1-dimensional over sectors
+        par.beta0 = np.array([0.008, 0.01, 0.012]) #1-dimensional over sectors
 
     def create_grids(self):
         """ grids for experience """
@@ -55,8 +59,8 @@ class GM:
         sol = self.sol
         par = self.par
 
-        sol.c = np.zeros((par.N_ages, par.T), dtype=int) - 99
-        sol.v = np.zeros((par.N_ages, par.T))
+        sol.c = np.zeros((par.N_ages, par.S, par.T)) - 1
+        sol.v = np.zeros((par.N_ages, par.T)) - 99
 
     # def value_of_choice(d, t, a):
     #     par = self.par
@@ -69,66 +73,76 @@ class GM:
         #Precompute wages (a x s x t) = (36, 3, 4)
         self.sol.w = np.transpose(self.sol.H)[:, :, np.newaxis] * self.par.r[np.newaxis, :, :]
 
+    @staticmethod
+    def EV_closedform(sigma, arr, axis=0):
+        return sigma * np.log(np.sum(np.exp(arr / sigma), axis))
+
+    #måske slå de her to sammen? så CCP og EV regnes samtidig, det er måske hurtigere?
+    @staticmethod
+    def CCP_closedform(sigma, arr, axis=0):
+        if arr.ndim <= 1:
+            return np.exp(arr / sigma) / np.sum(np.exp(arr / sigma))
+        else:
+            return np.divide(np.exp(arr / sigma), np.sum(np.exp(arr / sigma), axis)[:, np.newaxis])
+
     def solve(self):
         """ Solve model by backwards induction """
         par = self.par
         sol = self.sol
 
-        #Precompute
-        self.precompute()
-
         #Unpack parameters
-        r = par.r
+        #r = par.r
 
         #Unpack solution objects
-        H = sol.H
+        #H = sol.H
         w = sol.w
         c = sol.c
-        v = sol.v
+        v = sol.v #expected value functions
 
         #PART I (age iteration within the terminal period, static expectation)
         t = par.T - 1
+        a = par.N_ages - 1
 
         #Start at the retirement age, where there are no continuation values
-        V_alternatives = H[:, -1] * r[:, t]
-        c[-1, -1] = np.argmax(V_alternatives)
-        v[-1, -1] = V_alternatives[c[-1, -1]] #np.max(V_alternatives). Hvad er hurtigst af indexering og genregning?
+        v[a, t] = self.EV_closedform(par.sigma, w[a, :, t])
+        c[a, :, t] = self.CCP_closedform(par.sigma, w[a, :, t])
 
         #Then iterate from age 64 to 30.
+        #a = 34
+        a = 34
         for a in reversed(np.arange(par.N_ages - 1)):
-            V_alternatives = w[a, :, t] + par.rho*v[a + 1, t]
-            c[a, -1] = np.argmax(V_alternatives)
-            v[a, -1] = V_alternatives[c[a, -1]] #np.max(V_alternatives). Hvad er hurtigst?
+            V_alternatives = w[a, :, t] + par.rho * v[a + 1, t] #Nemt her stadig pga. simpel transformation function.
+                                                                #når valget i dag påvirker min state i morgen kommer det ind her
+            v[a, t] = self.EV_closedform(par.sigma, V_alternatives)
+            c[a, :, t] = self.CCP_closedform(par.sigma, V_alternatives)
 
         #PART II (time iteration from T - 2 to 0.)
         #Iterate from period T-2 (second to last period). Within each period. I don't have to iterate on age yet
+        t = par.T - 2
         for t in reversed(np.arange(par.T - 1)):
             #Calculate the value function when age = 65, which has no continuation value
             a = par.N_ages - 1
-            V_alternatives = w[a, :, t]
-            c[a, t] = np.argmax(V_alternatives)
-            v[a, t] = V_alternatives[c[a, t]]
+            v[a, t] = self.EV_closedform(par.sigma, w[a, :, t])
+            c[a, :, t] = self.CCP_closedform(par.sigma, w[a, :, t])
 
             #Calculate the value functions for ages 64 - 30
             #H[:, 0:par.N_ages - 1] * r[:, t][:, np.newaxis] bruger mindre memory men kan ikke precomputes. Hvad er bedst?
-            V_alternatives =  w[0:par.N_ages - 1, :, t] + par.rho*v[1:, t + 1][:, np.newaxis]
-            c[:-1, t] = np.argmax(V_alternatives, axis=1) #argmax along s dimension
-            v[0:-1, t] = V_alternatives[np.arange(V_alternatives.shape[0]), c[:-1, t]]
+            V_alternatives =  w[0:par.N_ages - 1, :, t] + par.rho*v[1:, t + 1][:, np.newaxis]    
+            v[0:-1, t] = self.EV_closedform(par.sigma, V_alternatives, axis=1)
+            c[0:-1, :, t] = self.CCP_closedform(par.sigma, V_alternatives, axis=1)
 
-        assert (c == np.argmax(w, axis=1)).all()
-
-        #experience er en skalar, som kan tage kontinuerte værdier. 
-        # 
-        # Så derfor skal vi bruge noget interpolation v. regression til at finde steder i statespacet
-        # som ikke findes endnu. Det skal nok gå. Måske lav 2 types også, så vi kan få nogle cross-terms
-        #?
+            #experience er en skalar, som kan tage kontinuerte værdier. 
+            # 
+            # Så derfor skal vi bruge noget interpolation v. regression til at finde steder i statespacet
+            # som ikke findes endnu. Det skal nok gå. Måske lav 2 types også, så vi kan få nogle cross-terms
+            #?
 
     def simulate(self):
         """Primitive function for simulating individuals forward"""
         c = self.sol.c
 
         #initialize 2 persons at each age and simulate 35 periods forward. Do not generate new cohorts.
-        #This means we create 36 cohorts and let the number of cohorts decrease by 1 each year.
+        #This means we create 36 cohorts and let the number of active cohorts decrease by 1 each year.
         N_sim = self.par.N_ages * 2
         ss = np.zeros((N_sim, self.par.T), dtype=int) - 1 #only 1-dimensional apart from N x T since the i-specific part of state space only consists of age
         sol = np.zeros((N_sim, self.par.T), dtype=int) - 1
@@ -176,7 +190,14 @@ gm = GM()
 gm.setup()
 gm.create_human_capital_unit_prices()
 gm.allocate()
+gm.precompute()
+
+#%%
 gm.solve()
+
+
+#%%
+
 gm.simulate()
 gm.fig_employment()
 
